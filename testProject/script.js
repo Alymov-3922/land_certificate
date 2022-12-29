b5fb7cc48c241300494729101977a4eb9b7ccef8
Alymov-3922 <alymovadilet1996@gmail.com>
2022-12-29 16:11:49 +0600

    qr_code

--- testProject/script.js
+++ testProject/script.js
@@ -7,28 +7,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //http://agromap.24mycrm.com:8111/contour/
 
-var qrcode = new QRCode("qrcode");
-
-function makeCode () {
-    var elText = document.getElementById("text");
-
-    if (!elText.value) {
-        alert("Input a text");
-        elText.focus();
-        return;
-    }
-
-    qrcode.makeCode(elText.value);
-}
-
-makeCode();
-
-$("#text").
-on("blur", function () {
-    makeCode();
-}).
-on("keydown", function (e) {
-    if (e.keyCode == 13) {
-        makeCode();
-    }
-});
+    var qrcode = new QRCode("qrcode",
+    "https://g-b.24mycrm.com");
