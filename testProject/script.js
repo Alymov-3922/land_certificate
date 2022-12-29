84f3b08e59bec9a26b64c4629ebba0af7107b717
Alymov-3922 <alymovadilet1996@gmail.com>
2022-12-29 12:28:35 +0600

    first_commit

---
+++ testProject/script.js
@@ -0,0 +1,34 @@
+let map = L.map('map').setView([42.87, 74.59], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//http://agromap.24mycrm.com:8111/contour/
+
+var qrcode = new QRCode("qrcode");
+
+function makeCode () {
+    var elText = document.getElementById("text");
+
+    if (!elText.value) {
+        alert("Input a text");
+        elText.focus();
+        return;
+    }
+
+    qrcode.makeCode(elText.value);
+}
+
+makeCode();
+
+$("#text").
+on("blur", function () {
+    makeCode();
+}).
+on("keydown", function (e) {
+    if (e.keyCode == 13) {
+        makeCode();
+    }
+});
